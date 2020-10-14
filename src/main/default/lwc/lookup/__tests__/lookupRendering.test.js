2c5f9df10cd4a51e2dc91e1ff9fe2fb59809cdff
Gorazd Berghaus <gorazd.berghaus@up2go.com>
2020-10-14 12:05:30 +0200

    Fixed a unit test and ignored linters for submodules

--- src/main/default/lwc/lookup/__tests__/lookupRendering.test.js
+++ src/main/default/lwc/lookup/__tests__/lookupRendering.test.js
@@ -88,8 +88,8 @@ describe('c-lookup rendering', () => {
         document.body.appendChild(element);
 
         // Verify selected icon
-        const selIcon = element.shadowRoot.querySelector('lightning-icon');
-        expect(selIcon.alternativeText).toBe('Selected item icon');
+        const selIcon = element.shadowRoot.querySelector('c-icon');
+        expect(selIcon.size).toBe('small');
         // Verify clear selection button
         const clearSelButton = element.shadowRoot.querySelector('button');
         expect(clearSelButton.title).toBe('Remove selected option');
