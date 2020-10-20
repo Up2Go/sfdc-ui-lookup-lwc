7f2c2084d604dcc28457a13e827bfc200f6f0ada
Gorazd Berghaus <gorazd.berghaus@up2go.com>
2020-10-20 11:48:20 +0200

    Removed resource icon

--- src/main/default/lwc/resourceIcon/resourceIcon.js
+++
@@ -1,9 +0,0 @@
-import resource from '@salesforce/resourceUrl/teamIcons';
-import Icon from 'c/icon';
-
-export default class ResourceIcon extends Icon {
-	constructor() {
-		super({"2Team": resource});
-	}
-}
-
